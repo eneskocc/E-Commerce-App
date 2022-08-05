a1aa2fef204b888020f09db05d239b82f0748d3d
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-05 11:07:59 +0300

    header yazıldı

--- client/app/component/BackHeader.js
+++ client/app/component/BackHeader.js
@@ -1,14 +1,22 @@
-import { Text, View,SafeAreaView,Image} from 'react-native'
-import React, { Component } from 'react'
+import {Text, View, SafeAreaView, Image} from 'react-native';
+import React, {Component} from 'react';
 
 export class BackHeader extends Component {
   render() {
     return (
       <SafeAreaView>
-        <Image style={{width: 20, height: 20,resizeMode:'contain',marginHorizontal:20}} source={require('../assets/images/home/back.png')} />
+        <Image
+          style={{
+            width: 20,
+            height: 20,
+            resizeMode: 'contain',
+            marginHorizontal: 20,
+          }}
+          source={require('../assets/images/home/back.png')}
+        />
       </SafeAreaView>
-    )
+    );
   }
 }
 
-export default BackHeader
+export default BackHeader;
--- client/app/screens/Welcome/Login.js
+++ client/app/screens/Welcome/Login.js
@@ -1,15 +1,19 @@
-import { Text, View,StyleSheet } from 'react-native'
-import React, { Component } from 'react'
-import BackHeader from '../../component/BackHeader'
+import {Text, View, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import BackHeader from '../../component/BackHeader';
+import Btn from '../../component/Btn';
 
 export class Login extends Component {
   render() {
     return (
       <View>
         <BackHeader />
+        <View style={{paddingHorizontal: '3%'}}>
+          <Btn txt={'SIGN UP'}/>
+        </View>
       </View>
-    )
+    );
   }
 }
 
-export default Login
+export default Login;
