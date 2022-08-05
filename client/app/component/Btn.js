81313ab873b215323d014981f2ef0f36fe09c6c3
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-05 11:08:32 +0300

    kırmızı renk button componenti yazıldı

---
+++ client/app/component/Btn.js
@@ -0,0 +1,15 @@
+import { Text, View,TouchableOpacity } from 'react-native'
+import React, { Component } from 'react'
+import { colors } from '../config'
+
+export class Btn extends Component {
+  render() {
+    return (
+      <TouchableOpacity style={{backgroundColor:colors.red,width:'100%',paddingVertical:10,borderRadius:25,alignItems:'center'}}>
+        <Text style={{color:colors.white,fontSize:14,lineHeight:20,fontWeight:'600'}}>{this.props.txt}</Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+export default Btn
--- client/app/config/index.js
+++ client/app/config/index.js
@@ -10,18 +10,8 @@ const screen = {
 const apiUrl='https://myapp.herokuapp.com'; 
 
 const colors = {
-    red:'#E01C3B',
-    tabGray:'#C7C5C5',
-    buttonGray:'#A9A9A9',
-    placeholderGray:'#969393',
-    white:'#FFFFFF',
-    darkblue:'#35495E',
-    balck:'#000000',
-    txtGray:'#8B8989',
-    txtGri:'#AFAFAF',
-    txtGriAcik:'#9E9E9E',
-    orange:'#FF6B00',
-    acikRed:'#FFE4E4',
+    red:'#DB3022',
+    white:'#ffffff'
 };
 
 
