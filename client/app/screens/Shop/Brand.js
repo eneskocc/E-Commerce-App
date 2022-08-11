92c697310506611d2a922e425f82b5bd04bb3cf7
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-11 22:12:29 +0300

    checkbox eklendi

--- client/app/screens/Shop/Brand.js
+++ client/app/screens/Shop/Brand.js
@@ -2,13 +2,41 @@ import {Text, View} from 'react-native';
 import React, {Component} from 'react';
 import BackHeader from '../../component/BackHeader';
 import SearchTextBox from '../../component/SearchTextBox';
+import BtnCheckBox from '../../component/BtnCheckBox';
+import BtnTransparan from '../../component/BtnTransparan';
+import Btn from '../../component/Btn';
+import { colors } from '../../config';
 
 export class Brand extends Component {
   render() {
     return (
       <View>
-        <BackHeader txt={'Brand'} />
+        <BackHeader
+          txt={'Brand'}
+          onClick={() => this.props.navigation.goBack()}
+        />
         <SearchTextBox />
+        <View style={{paddingHorizontal: '4%'}}>
+          <BtnCheckBox txt={'adidas'} />
+          <BtnCheckBox txt={'adidas Originals'} />
+          <BtnCheckBox txt={'Blend'} />
+          <BtnCheckBox txt={'Boutique Moschino'} />
+          <BtnCheckBox txt={'Champion'} />
+        </View>
+        <View
+          style={{
+            paddingHorizontal: '4%',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginVertical: 20,
+          }}>
+          <View style={{width: '45%'}}>
+            <BtnTransparan txt={'Discard'} border={colors.black} />
+          </View>
+          <View style={{width: '45%'}}>
+            <Btn txt={'Apply'} />
+          </View>
+        </View>
       </View>
     );
   }
