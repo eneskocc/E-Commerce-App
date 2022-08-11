4b1de779e72ddfe3d43e490ba529eaf33447124d
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-11 15:25:54 +0300

    catalog sayfası yazıldı

--- client/app/screens/Shop/Catalog.js
+++ client/app/screens/Shop/Catalog.js
@@ -1,18 +1,43 @@
-import {Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import React, {Component} from 'react';
 import SearchHeader from '../../component/SearchHeader';
 import ViewFiter from '../../component/ViewFiter';
-import { colors } from '../../config';
+import {colors} from '../../config';
 import ViewTags from '../../component/ViewTags';
+import ViewProductCard from '../../component/ViewProductCard';
 
 export class Catalog extends Component {
   render() {
     return (
       <View>
         <SearchHeader />
-        <View style={{paddingHorizontal: '4%',backgroundColor:colors.white}}>
+        <View style={{paddingHorizontal: '4%', backgroundColor: colors.white}}>
           <ViewTags />
           <ViewFiter />
+        </View>
+        <View style={{paddingHorizontal: '4%'}}>
+          <ScrollView>
+            <ViewProductCard
+              img={require('../../assets/images/product/img1.png')}
+              index={1}
+            />
+            <ViewProductCard
+              img={require('../../assets/images/product/img2.png')}
+              index={2}
+            />
+            <ViewProductCard
+              img={require('../../assets/images/product/img3.png')}
+              index={3}
+            />
+            <ViewProductCard
+              img={require('../../assets/images/product/img2.png')}
+              index={4}
+            />
+            <ViewProductCard
+              img={require('../../assets/images/product/img3.png')}
+              index={5}
+            />
+          </ScrollView>
         </View>
       </View>
     );
